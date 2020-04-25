7e87ecd1e2f3969dc478c918c4efa366faa398e4
kavinda ayesh <kavisocial01@gmail.com>
2020-04-25 11:44:51 +0530

    commit 1

---
+++ WebApp/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        infoEntities db = new infoEntities();
+
+        //GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user objchk)
+        {
+            if(ModelState.IsValid)
+            {
+                using (infoEntities db = new infoEntities())
+                {
+                    var obj = db.users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    else
+                    {
+                        ModelState.AddModelError("", " The UserName or Password is Incorrect");
+                    }
+
+                }
+
+            }
+
+            
+
+
+            return View(objchk);
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", " Login");
+        }
+
+    }
+}
